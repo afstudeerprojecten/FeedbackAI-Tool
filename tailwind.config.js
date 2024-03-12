079bb3478e7529ed358f241b33a542d70ae64b3d
AlecV <alec.vangilbergen@student.ucll.be>
2024-03-12 13:27:37 +0100

    Update TailwindCSS config to include content files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
-// tailwind.config.js
-module.exports = {
-  content: [],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [require('daisyui')],
-};
+}
