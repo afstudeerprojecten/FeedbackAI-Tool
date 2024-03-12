d284592edc60fe0ade8e7fe73ede0f3cad0996f0
AlecV <alec.vangilbergen@student.ucll.be>
2024-03-12 16:02:47 +0100

    Add TemplatesInterface component to FormPage

--- src/pages/FormPage.tsx
+++ src/pages/FormPage.tsx
@@ -1,12 +1,16 @@
 // FormPage.tsx
 import React from 'react';
 import AssignmentForm from '../components/AssignmentForm';
+import TemplatesInterface from '../components/TemplatesInterface';
 const FormPage: React.FC = () => {
   return (
     <div className="container mx-auto p-4">
       <div className="mb-8">
         <AssignmentForm />
       </div>
+      <div className='mb-8'>
+        <TemplatesInterface />
+    </div>
     </div>
   );
 };
