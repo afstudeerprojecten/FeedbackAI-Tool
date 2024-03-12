c4a8bad846ace6312c14cd217fdf7e0a06c946e0
AlecV <alec.vangilbergen@student.ucll.be>
2024-03-12 14:36:45 +0100

    Added Hero landing page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,13 +2,17 @@
 import React from 'react';
 
 const Home: React.FC = () => {
-  return (
-    <div className="container mx-auto mt-8">
-      <h1 className="text-4xl font-bold">Welcome to My React App</h1>
-      <p className="text-lg mt-4">This is the home page of the application.</p>
-      {/* Add more content as needed */}
-    </div>
-  );
+    return (
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content text-center">
+                <div className="max-w-md">
+                    <h1 className="text-5xl font-bold">Hello there</h1>
+                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <button className="btn btn-primary">Get Started</button>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Home;
