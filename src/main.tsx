5e8a675e9ed3c2ecca5f835eb9c213a145fdff8b
AlecV <alec.vangilbergen@student.ucll.be>
2024-03-12 12:07:06 +0100

    added initial navbar and homepage + installed tailwind and daisyui

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+// src/index.tsx
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+  document.getElementById('root')
+);
---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+// src/pages/Home.tsx
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div className="container mx-auto mt-8">
+      <h1 className="text-4xl font-bold">Welcome to My React App</h1>
+      <p className="text-lg mt-4">This is the home page of the application.</p>
+      {/* Add more content as needed */}
+    </div>
+  );
+};
+
+export default Home;
