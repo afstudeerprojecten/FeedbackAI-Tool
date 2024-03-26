d66400818f9ff5d0bdaf560f7b40ee79b6c41ee0
AlecV <alec.vangilbergen@student.ucll.be>
2024-03-26 17:16:07 +0100

    Added RegisterTeacher functionality + connection to the backend

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -34,7 +34,7 @@ const Navbar: React.FC = () => {
                         <>
                             <Link to="/chat" className="text-white hover:text-primary-light transition-colors duration-200">Student Submission</Link>
                             <Link to="/assignment" className="text-white hover:text-primary-light transition-colors duration-200">Create Assignment</Link>
-                            <Link to="/registeruser" className="text-white hover:text-primary-light transition-colors duration-200">Register User</Link>
+                            <Link to="/registerteacher" className="text-white hover:text-primary-light transition-colors duration-200">Register Teacher</Link>
                             <Link to="/registerorg" className="text-white hover:text-primary-light transition-colors duration-200">Register Organisation</Link>
                             <Link to="/organisations" className="text-white hover:text-primary-light transition-colors duration-200">Organisation Overview</Link>
                             <Link to="/registercourse" className="text-white hover:text-primary-light transition-colors duration-200">Register Course</Link>
