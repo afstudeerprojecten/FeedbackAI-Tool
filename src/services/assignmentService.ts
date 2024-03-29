a6d401ef4542cbdbc4d61bb60fa99cda062669c3
AlecV <alec.vangilbergen@student.ucll.be>
2024-03-29 14:46:42 +0100

    Implemented the assignment creation (without template integration for now)

---
+++ src/services/assignmentService.ts
@@ -0,0 +1,53 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:8000';  // Change this to your backend URL
+
+export const createAssignment = async (assignmentData: any) => {
+    try {
+        const config = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'  
+            },
+            mode: 'cors' // Ensure CORS mode
+        };
+        const response = await axios.post(`${API_URL}/assignment/add`, assignmentData, config);
+        return response.data;
+    } catch (error: any) {
+        throw error.response.data.detail || 'An error occurred while registering the assignment';
+    }
+};
+
+export const fetchAssigments = async () => {
+    try {
+        const config = {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'  
+            },
+            mode: 'cors' // Ensure CORS mode
+        };
+        const response = await axios.get(`${API_URL}/assignments`, config);
+        return response.data;
+    } catch (error: any) {
+        throw error.response.data.detail || 'An error occurred while fetching assignments';
+    }
+};
+
+// export const fetchAssignment = async (assignmentId: number) => {
+//     try {
+//         const config = {
+//             method: 'GET',
+//             headers: {
+//                 'Content-Type': 'application/json'  
+//             },
+//             mode: 'cors' // Ensure CORS mode
+//         };
+//         const response = await axios.get(`${API_URL}/assignment/${assignmentId}`, config);
+//         return response.data;
+//     } catch (error: any) {
+//         throw error.response.data.detail || 'An error occurred while fetching assignment';
+//     }
+// }
+
+
