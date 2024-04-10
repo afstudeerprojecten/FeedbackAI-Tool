b93e326e81008d285be34c69c4c5ef6d354f3244
AlecV <alec.vangilbergen@student.ucll.be>
2024-04-10 14:37:38 +0200

    Updated TemplateOverview to include button and turnicated text

--- src/pages/TemplateTableOverviewPage.tsx
+++ src/pages/TemplateTableOverviewPage.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import AssignmentTemplatesOverview from '../components/Templates/TemplateTableOverview';
+import { useNavigate } from 'react-router-dom';
 
 const TemplatesTableOverviewPage: React.FC = () => {
+
+
+  const navigate = useNavigate();
+  const handleGoBack = () => {
+    navigate('/assignments', { replace: true });
+};
   return (
     <div>
+      <button className="btn btn-primary m-4" onClick={handleGoBack}>Back to Overview</button>
       <AssignmentTemplatesOverview />
+
     </div>
   );
 };
