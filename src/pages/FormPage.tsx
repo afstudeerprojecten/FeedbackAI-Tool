245b0671b5384205d24e78932369d056a63e8219
AlecVangilbergen <alec.vangilbergen@student.ucll.be>
2024-06-11 15:27:19 +0200

    Updated notifications for users

--- src/pages/FormPage.tsx
+++ src/pages/FormPage.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import AssignmentForm from '../components/Assignment/AssignmentForm';
+import { ToastContainer } from 'react-toastify';
 
 const FormPage: React.FC = () => {
   return (
     <main className="min-h-screen bg-light-neutral dark:bg-dark-neutral">
       <div className="mb-8">
         <AssignmentForm />
+        <ToastContainer position="top-center" />
+
       </div>
     </main>
   );
