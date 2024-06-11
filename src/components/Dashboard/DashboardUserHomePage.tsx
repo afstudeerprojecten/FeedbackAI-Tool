e0ab2f9e299638f655c2846a46a2d051a3fa99eb
Seppe Van Vaerenbergh <r0751620@ucll.be>
2024-06-11 10:09:36 +0200

    update homepage analytics

--- src/components/Dashboard/DashboardUserHomePage.tsx
+++ src/components/Dashboard/DashboardUserHomePage.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { fetchStudentByEmail } from "../../services/studentService";
-import { fetchEventByUser } from "../../services/eventLogService";
+import { fetchEventByUser, fetchEventNameById } from "../../services/eventLogService";
 
 
 
@@ -9,24 +9,58 @@ const DashboardUserHomePage: React.FC = () => {
     const [user, setUser] = useState<any>({});
     useEffect(() => {
         const fetchData = async () => {
-            try {
+            const change_id_to_name = async (id: number) => {
+                try {
+                  const data = await fetchEventNameById(id);
+                  return data;
+                } catch (error: any) {
+                  console.error(error.message);
+                }
+              };
+
+              try {
                 const user = await fetchStudentByEmail(JSON.parse(sessionStorage.getItem('user') || '{}').email);
                 setUser(user);
-                const data = await fetchEventByUser(1);
-                setEvents(data);
+                const data = await fetchEventByUser(user.id);
+    
+                // Group and sum the events
+                const groupedEvents = await data.reduce(async (accPromise: Promise<any>, event: any) => {
+                    const acc = await accPromise;
+                    const eventName = await change_id_to_name(event.event_id);
+                    console.log(eventName.name);
+                    if (!eventName) {
+                        return acc; // Skip if event name not found
+                    }
+    
+                    const key = eventName.name; // Change 'type' to the attribute you want to group by
+                    if (!acc[key]) {
+                        acc[key] = { ...event, name: eventName.name, value: 0 }; // Initialize if not already present
+                    }
+                    acc[key].value += event.value; // Sum the count attribute
+                    return acc;
+                }, Promise.resolve({}));
+    
+                // Convert groupedEvents back to an array
+                const groupedEventsArray = Object.values(groupedEvents);
+                setEvents(groupedEventsArray);
             } catch (error: any) {
                 console.error(error.message);
             }
         };
-
         fetchData();
     }, []);
     
-    
 
     return (
-        <div className="text-6xl font-bold text-light-text dark:text-dark-text">
-            <h1 className="text-2xl font-bold p-6">Welcome back {user.name}</h1>
+        <div className="container">
+            <h1 className="text-6xl font-bold text-light-text dark:text-dark-text">Welcome back {user.name}</h1>
+            <ul>
+                {events.map((event, index) => (
+                    <li key={index}>
+                        {event.name}  :   {event.value}
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 };
