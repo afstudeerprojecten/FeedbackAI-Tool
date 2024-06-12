6d4b718c6b20313f2cccffa506f496c55e78b903
Sandeep Singh <sandeep159@outlook.com>
2024-06-12 06:42:32 +0200

    Added FileUploader component for selecting files to upload

---
+++ src/components/Course/FileUploader.tsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+
+interface FileUploaderProps {
+    selectedFile: File | null | undefined
+    handleFileSelect: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  }
+  
+
+const FileUploader: React.FC<FileUploaderProps> = ({ selectedFile, handleFileSelect }) => {
+  return (
+    <div className='mb-4'>
+        <label className="block text-sm font-bold mb-2" htmlFor="file">
+                Choose an PDF file
+        </label>
+      <input id="file" type="file" onChange={(e) => handleFileSelect(e)} />
+      {selectedFile && (
+        <div>
+          <p>Selected File:</p>
+          <p>{selectedFile.name}.{selectedFile.type}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default FileUploader;
