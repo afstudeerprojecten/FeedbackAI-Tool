a7d7fc6e03d7b0ed4e4183a5e5aab8755bbeed74
Sandeep Singh <sandeep159@outlook.com>
2024-06-15 17:19:51 +0200

    Styling fixes upload docs page

--- src/pages/UploadCourseDocumentsPage.tsx
+++ src/pages/UploadCourseDocumentsPage.tsx
@@ -3,16 +3,10 @@ import UploadDocumentsForm from '../components/Course/UploadDocumentsForm';
 
 const UploadCourseDocumentsPage: React.FC = () => {
   return (
-    <div className="container mx-auto p-4">
-      <div className="mb-8">
-        <div className="bg-base shadow-2xl rounded px-8 pt-6 pb-8 mb-4">
-          <h1 className="text-2xl font-bold mb-4 text-center">How To Use</h1>
-          <p className="text-center mb-4">1. Select the course you want to upload documents for from the dropdown.</p>
-          <p className="text-center mb-4">2. Upload your document.</p>
-        </div>
+    <main className="min-h-screen min-w-screen bg-light-neutral dark:bg-dark-neutral">
         <UploadDocumentsForm/>
-      </div>
-    </div>
+
+    </main>
   );
 };
 
